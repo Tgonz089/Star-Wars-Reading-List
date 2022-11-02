2e3cc8b817722febeadf1770d2d2163d11412c51
Tgonz089 <tristangonzales8936@gmail.com>
2022-11-02 01:15:25 +0000

    Finished the DELETE functions on api.

--- src/api/routes.py
+++ src/api/routes.py
@@ -7,6 +7,7 @@ from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
+
 @api.route('/user', methods=['GET'])
 def user_all():
     users = User.query.all()
@@ -59,6 +60,7 @@ def single_planet(planet_id):
         all_planets=[i.serialize() for i in single_planet]
     )
 
+
 @api.route('/vehicle', methods=['GET'])
 def vehicle_all():
 
@@ -76,7 +78,8 @@ def single_vehicle(vehicle_id):
         all_vehicles=[i.serialize() for i in single_vehicle]
     )
 
-@api.route('/users/favorites', methods= ['GET'])
+
+@api.route('/users/favorites', methods=['GET'])
 def get_fav_planet():
     users = User.query.all()
     for user in users:
@@ -86,50 +89,66 @@ def get_fav_planet():
             per_result = person.name
         for vehicle in user.favorite_vehicle:
             veh_result = vehicle.vehicle_name
-    return jsonify(result,per_result, veh_result)
+    return jsonify(result, per_result, veh_result)
 
-@api.route("/favorite/planet/<int:planet_id>", methods = ["POST"])
+
+@api.route("/planet/<int:planet_id>", methods=["POST"])
 def add_planet(planet_id):
     new = request.get_json()
     new_planet = Planet(id=planet_id, planet_name=new["planet_name"])
     db.session.add(new_planet)
     db.session.commit()
 
-    return jsonify("Planet added to favorites"),200
+    return jsonify("A Planet was added"), 200
 
-@api.route("/favorite/person/<int:person_id>", methods = ["POST"])
+
+@api.route("/person/<int:person_id>", methods=["POST"])
 def add_person(person_id):
     new = request.get_json()
     new_person = Person(id=person_id, name=new["name"])
     db.session.add(new_person)
     db.session.commit()
 
-    return jsonify("Person added to favorites"),200
+    return jsonify("A Person was added."), 200
 
-@api.route("/favorite/vehicle/<int:vehicle_id>", methods = ["POST"])
+
+@api.route("/vehicle/<int:vehicle_id>", methods=["POST"])
 def add_vehicle(vehicle_id):
     new = request.get_json()
     new_vehicle = Vehicle(id=vehicle_id, vehicle_name=new["vehicle_name"])
     db.session.add(new_vehicle)
     db.session.commit()
 
-    return jsonify("Vehicle added to favorites"),200
+    return jsonify("A Vehicle was added."), 200
 
-@api.route("/favorite/vehicle/<int:vehicle_id>", methods = ["DELETE"])
+
+@api.route('/vehicle/<int:vehicle_id>', methods=['DELETE'])
 def delete_vehicle(vehicle_id):
-    new = request.get_json()
-    deleted = Vehicle(vehicle_id)
-    db.session.deleted(deleted)
-    db.session.commit()
+    deleted_vehicle = Vehicle.query.get(vehicle_id)
+    if deleted_vehicle == None:
+        return ("This vehicle does not exist."), 200
+    else:
+        db.session.delete(deleted_vehicle)
+        db.session.commit()   
+        return jsonify("Vehicle was deleted."),200
 
-    return jsonify("Vehicle has been removed from favorites"),200
+@api.route('/person/<int:person_id>', methods=['DELETE'])
+def delete_person(person_id):
+    deleted_person = Person.query.get(person_id)
+    if deleted_person == None:
+        return ("This person does not exist."), 200
+    else:
+        db.session.delete(deleted_person)
+        db.session.commit()   
+        return jsonify("Person was deleted."),200
 
-@api.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+
+@api.route('/planet/<int:planet_id>', methods=['DELETE'])
 def delete_planet(planet_id):
-    deleted_planet = Planet.query.get(id=favorite_planet)
+    deleted_planet = Planet.query.get(planet_id)
     if deleted_planet == None:
-        return ("this vehicle does not exist"),200
+        return ("This planet does not exist."), 200
     else:
         db.session.delete(deleted_planet)
         db.session.commit()   
-        return jsonify("Planet deleted from the User's favorites."),200
+        return jsonify("Planet was deleted."),200
